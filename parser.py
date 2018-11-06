559d7d80b5677d6a694aa32578db842b01053ac2
YoTM <yolkin_t@list.ru>
2018-11-07 00:06:38 +0300

    Сервер скачивает файл и может передавать его имя на вход парсера. Парсер пока на Питоне Добавлена предобработка и поиск url-ов Нужно добавить пост обработку списка результатов через регулярки

--- parser.py
+++ parser.py
@@ -5,38 +5,54 @@ import csv
 import re
 
 
+def check_value(rlist, value):
+    """
+        пр-ра проверки значений строки записи файла
+        - проверка на перечисление url-ов
+    """
+    # если в ячейке записи url-лы перечисляются, т.е. их несколько
+    if re.search(r"[\,]", value):
+        # делим их по запятой и записываем в список
+        values = value.split(',')
+        for v in values:
+            rlist.append([v])
+    else:
+        rlist.append([value])
+
+
 def csv_parser(file_obj):
     """
         Процедура обработки строк файла адресов
     """
+    # читаем файл с данными
     data = csv.reader(file_obj, delimiter=';')
     # получаем список списков (строка файла данных = списку значений)
     data_list = [row for row in data]
 
-    # Обработка файла регулярками /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/
+    # Обработка файла регулярками
+    # проверка на url   /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/
+    # проверка на IP    /^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$/
 
+    print('Start parsing...')
 
-
-    # Запись результата парсинга в файл
-
-    # with open("output.txt", "w") as file:
-        # for item in data_list:
-        #     print(item, file=file)
-
-    res_list = []
-
+    urls_list = []
 
     for item in data_list:
-        for val in item:
-            # print(val)
-            if re.search(r"[*]", val) or re.search(r"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$", val):
-                print(val)
-                res_list.append(val)
+
+        if item[1] != '':
+            check_value(urls_list, item[1])
+
+        elif item[2] != '':
+            urls_list.append([item[2]])
 
     print("Parsing complete...")
 
-    with open("result.csv", "w", newline='') as file:
-        csv.writer(file).writerow(res_list)
+    with open("result.csv", "w") as file:
+        writer = csv.writer(file, delimiter=';')
+        for line in urls_list:
+            writer.writerow(line)
+
+    print("file output.csv is ready...")
 
 
 if __name__ == "__main__":
